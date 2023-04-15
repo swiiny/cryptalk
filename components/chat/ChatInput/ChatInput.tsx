e3e9b8a441fc3b84a84be94660ee6079d378a8c5
Jeremy Theintz <jeremy@appogee.ch>
2023-04-15 02:11:37 +0200

    try things using dialogflow api

--- components/chat/ChatInput/ChatInput.tsx
+++ components/chat/ChatInput/ChatInput.tsx
@@ -1,7 +1,5 @@
-import { Button } from '@components/shared/Button/Button';
 import { pushNewMessage } from '@hooks/chat/useMessagesQuery/useMessagesQuery';
 import { IMessage } from '@hooks/chat/useMessagesQuery/useMessagesQuery.type';
-import { useOpenAIMutation } from '@hooks/chat/useOpenAIMutation/useOpenAIMutation';
 import { useQueryClient } from '@tanstack/react-query';
 import { FC, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
@@ -85,8 +83,9 @@ enum EUser {
 
 const ChatInput: FC<IChatInput> = () => {
 	const queryClient = useQueryClient();
-	const openAIMutation = useOpenAIMutation();
-	const [inputType, setInputType] = useState<EInputType>(EInputType.selectAction);
+	const [message, setMessage] = useState('');
+	//const openAIMutation = useOpenAIMutation();
+	//const [inputType, setInputType] = useState<EInputType>(EInputType.selectAction);
 
 	function pushMessage(user: EUser, message: string) {
 		const newMessage: IMessage = {
@@ -99,142 +98,41 @@ const ChatInput: FC<IChatInput> = () => {
 		pushNewMessage(newMessage, queryClient);
 	}
 
-	async function getUserInput(aiResponse: string): Promise<string> {
-		pushMessage(EUser.bot, formatResponse(aiResponse));
-
-		return new Promise((resolve) => {
-			function handleKeyPress(e) {
-				if (e.key === 'Enter') {
-					const inputElement = document.getElementById('chat-input') as HTMLInputElement;
-					const message = inputElement.value;
-
-					pushMessage(EUser.user, message);
-
-					document.removeEventListener('keypress', handleKeyPress);
-
-					resolve(message);
-					inputElement.value = '';
-				}
-			}
-
-			document.addEventListener('keypress', handleKeyPress);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const response = await fetch('/api/dialogflow', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ message })
 		});
-	}
 
-	async function collectSwapInformation(initialUserInput: string): Promise<void> {
-		let conversationHistory = '';
-
-		// Get the initial user input
-		console.log('Please enter your swap request:');
-		conversationHistory += `${EUser.user}: ${initialUserInput}\n`;
-
-		// Let GPT-3 handle the conversation
-		const initialPrompt = `User wants to execute a swap. They said: '${initialUserInput}'.\n`;
-		let currentPrompt = initialPrompt;
-		let userResponse = '';
-		let userIntent: TCurrentUserInfo | null = null;
-
-		pushMessage(EUser.user, initialUserInput);
-
-		async function askOpenAi() {
-			console.log('start iterations');
-			let newPrompt = '';
-
-			let newQuestion =
-				currentPrompt +
-				" How should the AI bot ask the user for any missing swap data (tokenA, tokenB, amount, slippage)? Make sure to include a code-parsable string at the end of your response with user selected values or null in the format 'responseForCode: tokenA, tokenB, amount, slippage'. ";
-
-			const { tokenA, tokenB, amount, slippage } = userIntent || {
-				tokenA: null,
-				tokenB: null,
-				amount: null,
-				slippage: null
-			};
-
-			/* 		conversationHistory += `Context: The user wants to swap tokens. So far, they have provided the following information:\n`;
-			conversationHistory += `TokenA: ${tokenA}\n`;
-			conversationHistory += `TokenB: ${tokenB}\n`;
-			conversationHistory += `Amount: ${amount}\n`;
-			conversationHistory += `Slippage: ${slippage}\n`;
-
-			console.log('conversationHistory', conversationHistory);
-			newPrompt = `${conversationHistory}${newQuestion} `; */
-
-			let contextSummary = '';
-			contextSummary += `Context: The user wants to swap tokens. So far, they have provided the following information:\n`;
-			contextSummary += `TokenA: ${tokenA}\n`;
-			contextSummary += `TokenB: ${tokenB}\n`;
-			contextSummary += `Amount: ${amount}\n`;
-			contextSummary += `Slippage: ${slippage}\n`;
-
-			console.log('contextSummary', contextSummary);
-			newPrompt = `${contextSummary}${newQuestion} `;
-
-			openAIMutation.mutate(newPrompt, {
-				onSuccess: async (aiResponse) => {
-					console.log('GPT-3:', aiResponse);
-					userResponse = await getUserInput(aiResponse);
-					console.log('userResponse', userResponse);
-					conversationHistory += `${EUser.bot}: ${aiResponse}\n${EUser.user}: ${userResponse}\n`;
-
-					userIntent = extractUserIntentFromResponse(aiResponse);
-
-					// check if the user has confirmed the information
-					if (userResponse.includes('CONFIRMED')) {
-						alert("it's confirmed");
-						// can build the tx
-						return;
-					}
-
-					if (isComplete(userIntent)) {
-						// can build the tx
-						return;
-					}
-
-					//currentPrompt = `Continue the conversation based on the user's response: '${userResponse}'. `;
-					currentPrompt = `Continue the conversation based on the user's response: '${userResponse}'. If the user has confirmed the information, automatically include the word "CONFIRMED" in your response. `;
-
-					askOpenAi();
-				}
-			});
+		if (response.ok) {
+			const data = await response.json();
+			console.log('Intent:', data.intent);
+			console.log('Token A:', data.tokenA);
+		} else {
+			console.error('Error:', await response.text());
 		}
 
-		askOpenAi();
-	}
-
-	if (inputType === EInputType.selectAction) {
-		return (
-			<Button
-				onClick={() => {
-					collectSwapInformation('I want to swap tokens');
-					setInputType(EInputType.swap);
-				}}
-			>
-				Swap
-			</Button>
-		);
-	}
+		setMessage('');
+	};
 
 	return (
 		<StyledChatInput>
 			<form
 				onSubmit={(e) => {
 					e.preventDefault();
-					// scroll to bottom of chat feed with animation
-					/* const id = 'chat-feed-container';
-					const element = document.getElementById(id);
-
-					if (element) {
-						element.scrollIntoView({ behavior: 'smooth' });
-					} */
-
-					// trigger enter keypress event (even ig submit button is clicked)
-					const inputElement = document.getElementById('chat-input') as HTMLInputElement;
-					const event = new KeyboardEvent('keypress', { key: 'Enter' });
-					inputElement.dispatchEvent(event);
+					pushMessage(EUser.user, message);
+					handleSubmit(e);
 				}}
 			>
-				<input id='chat-input' type='text' placeholder='I want to swap tokens...' />
+				<input
+					id='chat-input'
+					type='text'
+					placeholder='I want to swap tokens...'
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+				/>
 				<button type='submit'>Send</button>
 			</form>
 		</StyledChatInput>
---
+++ pages/api/dialogflow.ts
@@ -0,0 +1,56 @@
+import dialogflow from '@google-cloud/dialogflow';
+
+const projectId = process.env.DIALOGFLOW_PROJECT_ID || '';
+//const sessionId = 'unique-session-id'; // You can generate a unique ID for each user session
+const languageCode = 'en-US';
+
+// import credentials from '../../private/appogee-dev-api-10fa1401f35c.json';
+import credentials from '../../private/appogee-dev-api-215ed7f0fb90.json';
+console.log(credentials);
+
+//const sessionClient = new dialogflow.SessionsClient();
+
+async function sendMessageToDialogflow(text: string) {
+	if (!projectId) {
+		throw new Error('Project ID is required');
+	}
+
+	//const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+	const sessionClient = new dialogflow.SessionsClient({ projectId, credentials });
+	const sessionId = 'some-random-id'; // Change this for different users/sessions
+	const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+	const request = {
+		session: sessionPath,
+		queryInput: {
+			text: {
+				text,
+				languageCode
+			}
+		}
+	};
+
+	const responses = await sessionClient.detectIntent(request);
+	const result = responses[0].queryResult;
+
+	// Extract the intent and the Token A value
+	//const intent = result.intent.displayName;
+	//const tokenA = result.parameters.fields['TokenA'].stringValue;
+
+	return result;
+}
+
+export default async function handler(req, res) {
+	if (req.method === 'POST') {
+		const { message } = req.body;
+		try {
+			const response = await sendMessageToDialogflow(message);
+			res.status(200).json(response);
+		} catch (error) {
+			console.error('Dialogflow error:', error);
+			res.status(500).json({ error: 'Internal server error' });
+		}
+	} else {
+		res.status(405).json({ error: 'Method not allowed' });
+	}
+}
